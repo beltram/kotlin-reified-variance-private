1d8aabc4ef799a75f637679dc17f590091e57989
beltram.maldant <beltram.maldant@soprabanking.com>
2018-10-23 19:59:47 +0200

    initial commit

---
+++ src/main/kotlin/com/beltram/Main.kt
@@ -0,0 +1,34 @@
+package com.beltram
+
+inline fun <reified T> T.myName(): String = T::class.simpleName ?: "simpleName"
+
+class User<out T>() {
+    // this FAILS
+    // with Type checking has run into a recursive problem. Easiest workaround: specify types of your declarations explicitly
+    private val myName = myName()
+
+//    this works
+//    protected val myName = myName()
+//
+//    this works
+//    internal val myName = myName()
+//
+//    this works
+//    public val myName = myName()
+}
+
+class Person<T>() {
+    // this works
+    private val myName = myName()
+
+//    this works
+//    protected val myName = myName()
+//
+//    this works
+//    internal val myName = myName()
+//
+//    this works
+//    public val myName = myName()
+}
+
+

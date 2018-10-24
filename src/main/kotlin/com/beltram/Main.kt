f149e69571c07f2ed075b2ab613b3c2de5f879da
beltram.maldant <beltram.maldant@soprabanking.com>
2018-10-24 11:22:27 +0200

    update following recommendations

--- src/main/kotlin/com/beltram/Main.kt
+++ src/main/kotlin/com/beltram/Main.kt
@@ -1,35 +1,29 @@
 package com.beltram
 
-inline fun <reified T> T.myName(): String = T::class.simpleName ?: "simpleName"
+inline fun <reified U> U.myName(): String = U::class.simpleName ?: "simpleName"
 
 class User<out T>() {
+
+    companion object {
+        // this works
+        private val myName = myName()
+    }
+
     // this FAILS
     // with Type checking has run into a recursive problem. Easiest workaround: specify types of your declarations explicitly
     private val myName = myName()
+
+    // this works with explicit type
+    private val myName: String = myName().invoke()
 
     // this works
     private val myOtherName = myName()
 
 //    this works
-//    protected val myName = myName()
-//
-//    this works
-//    internal val myName = myName()
-//
-//    this works
-//    public val myName = myName()
+//    (either public or internal or protected) val myName = myName()
 }
 
 class Person<T>() {
     // this works
     private val myName = myName()
-
-//    this works
-//    protected val myName = myName()
-//
-//    this works
-//    internal val myName = myName()
-//
-//    this works
-//    public val myName = myName()
 }
